4f1016f5a11be76fe2bf9c3c86b3e48a80b90aeb
MartaAzaanoune <marta.azaanoune2016@gmail.com>
2021-07-27 18:10:15 +0200

    init commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import Test from './components/Test'
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <Test/>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Marta Test
+
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
